2a106aee3fd19297d587e5f342724858308181b7
brunosandri <brunomsandri@gmail.com>
2025-07-05 10:20:57 -0400

    Atualiza estrutura do login com nova logo e layout refinado

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function login() {
   const password = document.getElementById("password").value;
 
   try {
-    const res = await fetch("http://localhost:5000/login", {
+    const res = await fetch("https://salusopp-backend.railway.internal", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
